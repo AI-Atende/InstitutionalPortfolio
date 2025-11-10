ef2f09a15c3003e64895e5758983c2d3acbc33e9
AlanMatheus <matheusalan334@gmail.com>
2025-11-09 22:07:57 -0300

    bottons altered

--- client/src/components/ResultsSection.tsx
+++ client/src/components/ResultsSection.tsx
@@ -105,7 +105,7 @@ export default function ResultsSection() {
             rel="noopener noreferrer"
           >
             <button className="inline-block bg-[#0f8516] text-white font-montserrat font-bold py-4 px-8 rounded-full text-lg transition-all duration-300 hover:scale-105 shadow-xl">
-              Agende uma Avaliação Gratuita
+              Agende uma Avaliação
             </button>
           </a>
         </div>
