9122ccbad3f689c914c1b2666c1a04eb09336652
AlanMatheus <matheusalan334@gmail.com>
2025-11-09 22:01:43 -0300

    Name bottom altered

--- client/src/components/Sobre.tsx
+++ client/src/components/Sobre.tsx
@@ -20,7 +20,7 @@ const Sobre = () => {
               rel="noopener noreferrer"
             >
               <button className="bg-[#0f8516] text-white ml-6 md:ml-0 font-montserrat font-bold py-4 px-8 rounded-full text-lg transition-all duration-300 hover:scale-105 shadow-xl">
-                Agende uma Avaliação Gratuita
+                Agende uma Avaliação
               </button>
             </a>
           </div>
